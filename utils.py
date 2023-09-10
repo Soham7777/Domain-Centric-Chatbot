2ab933778b28cff2fe5aaa8f64b54a010c2a29f3
Soham7777 <sohamsawant7@gmail.com>
2023-09-10 16:50:12 +0530

    file to define env variables

---
+++ utils.py
@@ -0,0 +1,5 @@
+OpenAI_api_key = "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"
+
+Mongo_cluster_URI=['mongodb+srv://chatbot-user:iA8QzmwhvpxOgufP@cluster0.iebkudb.mongodb.net/?retryWrites=true&w=majority']
+
+Urls = ['https://news.yahoo.com/','https://economictimes.indiatimes.com/markets']
