0b14875291de477ab419f8234c907d75f209ee14
Soham7777 <sohamsawant7@gmail.com>
2023-09-10 16:55:07 +0530

    Streamlit app runner

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from Model_VectorDB import get_answer
+
+
+#Starting 
+st.title("Hey There ! Let me be your chatbot assistant and update you with the latest Financial News")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Ask me about the market"):
+
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    #generate response from the model and store in the response variable
+    response = get_answer(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
